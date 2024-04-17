5a0362c5b9ab2cb1178045cedd830fa4c9c6a270
IC3lemon <150153966+IC3lemon@users.noreply.github.com>
2024-04-17 13:27:33 +0530

    Create rsa-basic.py

---
+++ scripts/rsa-basic.py
@@ -0,0 +1,20 @@
+import sympy as sp
+from Crypto.Util.number import *
+
+#  ----------rsa when p,q,c,e are available----------
+
+#  Thine must have sympy and pycryptodome modules installed for this to worketh.
+
+
+e = 65537
+ct = b'encoded'  # thine ciphertext in bytes
+c = bytes_to_long(cb) # thine ciphertext in long
+p = 6969 # thy prime p
+q = 6969 # thy prime q
+
+N = p*q
+phi = (p-1)*(q-1)
+d = inverse(e,phi)
+m = pow(c,d,N)
+
+THINE_MESSAGE = long_to_bytes(m) # ----THINE MESSAGE HAST BEEN FOUND-----
