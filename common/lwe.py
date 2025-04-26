20556131464fc35b8e91ba7f934b7ec726737fa6
ic3d_lemon <150153966+IC3lemon@users.noreply.github.com>
2025-04-27 01:15:23 +0530

    Create lwe.py

---
+++ common/lwe.py
@@ -0,0 +1,15 @@
+def lwe(A, b, p):
+    """
+    A * s + e = b (mod p)
+    given matrix A (m x n) of n eqns with m unknowns, b = [b_i ... ]
+    returns list [e_i ... ]
+    for small errors e
+    """
+    A = A.change_ring(ZZ)
+    b = b.change_ring(ZZ)
+    M = Matrix(ZZ, list(b), sparse=False)
+    M = M.stack(Matrix(ZZ, A.T, sparse=False)) 
+    M = M.stack(diagonal_matrix(ZZ, [p] * A.nrows(), sparse=False)) 
+    L = M.LLL()
+    for i in L:
+        print(f"{i}\n")
